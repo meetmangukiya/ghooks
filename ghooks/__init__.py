7eb751812c7841291579e36a9600e896db75dcd7
Meet Mangukiya <meet123mangukiya@gmail.com>
2017-01-27 20:55:59 +0530

    Basic Plugin framework to handle the hook data

---
+++ ghooks/__init__.py
@@ -0,0 +1,46 @@
+EVENTS = {
+    "commit_comment": [],
+    "create": [],
+    "delete": [],
+    "deployment": [],
+    "deployment_status": [],
+    "fork": [],
+    "gollum": [],
+    "issue_comment": [],
+    "issues": [],
+    "label": [],
+    "member": [],
+    "membership": [],
+    "milestone": [],
+    "organization": [],
+    "page_build": [],
+    "project_column": [],
+    "project": [],
+    "public": [],
+    "pull_request_review_comment": [],
+    "pull_request_review": [],
+    "pull_request": [],
+    "push": [],
+    "repository": [],
+    "release": [],
+    "status": [],
+    "team": [],
+    "team_add": [],
+    "watch": []
+}
+
+def events(*args, **kwargs):
+    def wrapper(func):
+        for event in args:
+            if event in EVENTS:
+                EVENTS[event].append(func)
+            else:
+                raise ValueError("Unrecognized event")
+        return func
+    return wrapper
+
+def call_handlers(data, event):
+    for handlers in EVENTS[event]:
+        handlers(data)
+
+from ghooks.hooks import main
---
+++ ghooks/hooks.py
@@ -0,0 +1,31 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--port', '-p')
+
+args = parser.parse_args()
+
+import os
+
+from ghooks import EVENTS, call_handlers
+
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/ghooks')
+def hook(methods=["POST"]):
+    gh_event = request.headers.get('X-GitHub-Event')
+    gh_signature = request.headers.get('X-Hub-Signature')
+    gh_id = request.headers.get('X-GitHub-Delivery')
+
+    data = request.form
+
+    call_handlers(data, gh_event)
+
+def run():
+    app.run(debug=True, port=args.port if args.port else 8000)
+
+if __name__ == "__main__":
+    main()
