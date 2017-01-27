ed0ef87d3fe03927cdfd97ee1d0efe62273259ee
Meet Mangukiya <meet123mangukiya@gmail.com>
2017-01-27 21:17:53 +0530

    Add requirements.py and include it in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import find_packages, setup
 with open("README.rst") as f:
     long_description = f.read()
 
+with open("requirements.txt") as f:
+    requirements = f.read()
+
 setup(name = 'ghooks',
     version = '0.1',
     description = "GitHub hooks made simpler to handle!",
@@ -12,5 +15,6 @@ setup(name = 'ghooks',
     url="https://github.com/meetmangukiya/ghooks",
     license = "MIT",
     packages=find_packages(),
+    install_requires=requirements,
     entry_points={}
     )
