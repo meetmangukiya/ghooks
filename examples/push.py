2b55a13096952cc514dbf404a7990d6e7757e725
Meet Mangukiya <meet123mangukiya@gmail.com>
2017-05-06 14:27:11 +0530

    examples/push: Import app instead of run

--- examples/push.py
+++ examples/push.py
@@ -1,8 +1,9 @@
 import ghooks
+from ghooks import app
 
 @ghooks.events('push')
 def push_event(data):
     print("Push event triggered!")
 
 if __name__ == "__main__":
-    ghooks.run()
+    app.run()
